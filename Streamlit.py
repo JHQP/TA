b0e5b927a8365575365830396345223e709c8de4
JHQP <137485609+JHQP@users.noreply.github.com>
2023-06-23 15:45:41 +0700

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,4 +1,152 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import pyrebase
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+config = {
+    "apiKey": "AIzaSyBu3Lfwo_HdNGLtFFCCEXdDssgAVcG1ShA",
+    "authDomain": "drinkingwaterquality-fab02.firebaseapp.com",
+    "databaseURL": "https://drinkingwaterquality-fab02-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "drinkingwaterquality-fab02",
+    "storageBucket": "drinkingwaterquality-fab02.appspot.com",
+    "messagingSenderId": "313523960550",
+    "appId": "1:313523960550:web:f9a2bbb83f7d8a6f0ad927",
+    "measurementId": "G-YEDZLEWDK2"
+  }
+
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
+
+EColi = database.child("WATER QUALITY").child("MIKROBIOLOGI PARAMETERS").child("E COLI").get().val()
+Coliform = database.child("WATER QUALITY").child("MIKROBIOLOGI PARAMETERS").child("COLIFORM").get().val()
+
+Arsen = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("ARSEN").get().val()
+TotalKromium = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("TOTAL KRONIUM").get().val()
+Kadmium = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("KADMIUM").get().val()
+Nitrit = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("NITRIT").get().val()
+Nitrat = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("NITRAT").get().val()
+Selenium = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("SELENIUM").get().val()
+Aluminium = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("ALUMINIUM").get().val()
+Sulfat = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("SULFAT").get().val()
+Tembaga = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("TEMBAGA").get().val()
+BOD = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("BOD").get().val()
+COD = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("COD").get().val()
+Amonia = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("AMONIA").get().val()
+Besi = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("BESI").get().val()
+SisaChlor = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("SISA CHLOR").get().val()
+Sianida = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("SIANIDA").get().val()
+Seng = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("SENG").get().val()
+Mangan = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("MANGAN").get().val()
+Klorida = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("KLORIDA").get().val()
+KesadahanTotal = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("KESADAHAN TOTAL").get().val()
+
+Bau = database.child("WATER QUALITY").child("PHYSICS PARAMETERS").child("BAU").get().val()
+Rasa = database.child("WATER QUALITY").child("PHYSICS PARAMETERS").child("RASA").get().val()
+Warna = database.child("WATER QUALITY").child("PHYSICS PARAMETERS").child("WARNA").get().val()
+
+pH = database.child("WATER QUALITY").child("CHEMICAL PARAMETERS").child("PH").get().val()
+TDS = database.child("WATER QUALITY").child("PHYSICS PARAMETERS").child("TDS").get().val()
+Turbidity = database.child("WATER QUALITY").child("PHYSICS PARAMETERS").child("TURBIDITY").get().val()
+Suhu = database.child("WATER QUALITY").child("PHYSICS PARAMETERS").child("SUHU").get().val()
+
+Uji = np.array([EColi,
+        Coliform,
+        Arsen,
+        TotalKromium,
+        Kadmium,
+        Nitrit,
+        Nitrat,
+        Selenium,
+        Aluminium,
+        pH,
+        Sulfat,
+        Tembaga,
+        TDS,
+        Turbidity])
+
+uji = Uji.astype(float)
+
+df = pd.DataFrame(Uji)
+df = df.T
+df = pd.DataFrame(df, columns = ['E.Coli', 'Coliform', 'Arsen', 'Total Kromium', 'Kadmium', 'Nitrit', 'Nitrat', 'Selenium', 'Aluminium', 'pH', 'Sulfat', 'Tembaga', 'TDS', 'Turbidity'], index=[''])
+
+
+
+data = pd.read_csv("https://raw.githubusercontent.com/JHQP/TA/main/Hasil%20Pengujian%20Kualitas%20Air%20PDAM%20-%20DataFrame.csv", index_col=0)
+
+dfs = data.drop(['Kelayakan'], axis='columns')
+dfs['E.Coli']= dfs['E.Coli'].str.replace(',','.')
+dfs['Coliform']= dfs['Coliform'].str.replace(',','.')
+dfs['Arsen (As)']= dfs['Arsen (As)'].str.replace(',','.')
+dfs['Total Kromium (Cr-T)']= dfs['Total Kromium (Cr-T)'].str.replace(',','.')
+dfs['Kadmium (Cd)']= dfs['Kadmium (Cd)'].str.replace(',','.')
+dfs['Nitrit (NO2-N)']= dfs['Nitrit (NO2-N)'].str.replace(',','.')
+dfs['Nitrat (NO3-N)']= dfs['Nitrat (NO3-N)'].str.replace(',','.')
+dfs['Selenium (Se)']= dfs['Selenium (Se)'].str.replace(',','.')
+dfs['Alumunium (Al)']= dfs['Alumunium (Al)'].str.replace(',','.')
+dfs['pH']= dfs['pH'].str.replace(',','.')
+dfs['Sulfat (SO4)']= dfs['Sulfat (SO4)'].str.replace(',','.')
+dfs['Tembaga (Cu)']= dfs['Tembaga (Cu)'].str.replace(',','.')
+dfs['Padatan Terlarut Total (TDS)']= dfs['Padatan Terlarut Total (TDS)'].str.replace(',','.')
+dfs['Kekeruhan']= dfs['Kekeruhan'].str.replace(',','.')
+
+dfs = dfs.astype(float)
+
+dfs['Kelayakan'] = data['Kelayakan']
+
+dfs = dfs.dropna()
+
+X=dfs.drop(['Kelayakan'], axis='columns').to_numpy()
+y=dfs['Kelayakan'].to_numpy()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, train_size=0.8)
+
+
+def elm_fit(X, target, h, W=None):
+    if W is None:
+        W = np.random.uniform(-.1, .1, (h, len(X[0])))
+
+    print('Hinit')
+    Hinit = X @ W.T
+    print(Hinit)
+    print('H')
+    H = 1 / (1 + np.exp(-Hinit))
+    print(H)
+    Ht = H.T
+    print('H+')
+    Hp = np.linalg.inv(Ht @ H) @ Ht
+    print(Hp)
+    print('beta')
+    beta = Hp @ target
+    print(beta)
+    print('y')
+    y = H @ beta
+    print(y)
+    mape = sum(abs(y - target) / target) * 100 / len(target)
+
+    return W, beta, mape
+
+def elm_predict_test(uji, W, b, round_output=False):
+    Hinit = X @ W.T
+    H = 1 / (1 + np.exp(-Hinit))
+    y = H @ b
+
+    if round_output:
+        y = [int(round(x)) for x in y]
+
+    return y
+
+W, b, mape = elm_fit(X_train, y_train, 10)
+predict = elm_predict_test(uji, W, b, round_output=True)
+
+
+
+#st.set_page_config(layout="wide")
+
+import streamlit as st
 
 col1, col2, col3 = st.columns([1, 3, 1])
 

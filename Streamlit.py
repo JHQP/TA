fbbfd348ff69412d8f1c7d38e135d33ef1edd962
JHQP <137485609+JHQP@users.noreply.github.com>
2023-06-23 15:29:37 +0700

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,9 +1,12 @@
+pip3 install pyrebase
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 from pyrebase import pyrebase
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
 
 config = {
     "apiKey": "AIzaSyBu3Lfwo_HdNGLtFFCCEXdDssgAVcG1ShA",

f5c5e450be909edcc79510dd264dd23b28b16b45
JHQP <137485609+JHQP@users.noreply.github.com>
2023-06-23 14:52:09 +0700

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ptiBxHeNGCSeckK7-hV-xAAg95SpYtRl
 """
 
-pip install pyrebase4
-
 import streamlit as st
 import pandas as pd
 import numpy as np

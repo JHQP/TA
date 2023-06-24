3bb5ffc5ab629a9089bf9c1515aac72f4d53e859
JHQP <137485609+JHQP@users.noreply.github.com>
2023-06-24 09:49:45 +0700

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pyrebase
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 config = {

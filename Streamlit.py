3ef3b1e2e0c3202f330c41deb923675a634fe2f0
JHQP <137485609+JHQP@users.noreply.github.com>
2023-07-24 12:54:19 +0700

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -144,10 +144,10 @@ import streamlit as st
 col1, col2 = st.columns([1, 4])
 
 with col1:
-  st.image('https://raw.githubusercontent.com/JHQP/TA/38ab24b214531d9a4292a0b616ae281d1dd30da1/Logo.png', width=180)
+  st.image('https://raw.githubusercontent.com/JHQP/TA/38ab24b214531d9a4292a0b616ae281d1dd30da1/Logo.png', width=150)
 
 with col2:
-  st.title('Kualitas Air Minum')
+  st.title('ADeQu - Kualitas Air Minum')
 
 
 st.write("""
